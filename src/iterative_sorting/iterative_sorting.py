d778910d19a31d0e2ab8f3bca5ea19a9e040cdcf
Daanish Rasheed <daanishrasheed@gmail.com>
2020-05-20 10:49:56 -0400

    done with sorting algorithms

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,10 +7,14 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
 
         # TO-DO: swap
         # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
@@ -18,6 +22,11 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
     return arr
@@ -26,6 +35,20 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
+    o = [0 for i in range(256)]
+    c = [0 for i in range(256)]
+    a = ["" for _ in arr]
+    for i in arr:
+        c[ord(i)] += 1
 
+    for i in range(256):
+        c[i] += c[i-1]
+
+    for i in range(len(arr)):
+        o[c[ord(arr[i])]-1] = arr[i]
+        c[ord(arr[i])] -= 1
+
+    for i in range(len(arr)):
+        arr[i] = o[i]
 
     return arr
